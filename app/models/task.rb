447643f78d3ced497298d5e7ede599c334b9ce23
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2025-04-11 12:06:52 +0200

    added validations for the task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,10 +4,26 @@ class Task < ApplicationRecord
   has_many :task_tags
   has_many :tags, through: :task_tags
 
-  validates :title, presence: true
+  enum :status, {
+    planned: 0,
+    active: 1,
+    completed: 2,
+    cancelled: 3,
+    on_hold: 4
+  }
+
+  enum :priority, {
+    low: 0,
+    medium: 1,
+    high: 2
+  }
+
+  validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 500 }
-  validates :priority, inclusion: { in: 0..5 }
-  validates :status, inclusion: { in: 0..5 }
+  # validate duedate correct date time value
+  # validate sheduled start format
   validates :estimated_duration, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :spent_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :priority, inclusion: { in: priorities.keys }
+  validates :status, inclusion: { in: statuses.keys }
 end
